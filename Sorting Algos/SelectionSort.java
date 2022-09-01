f914f5a19a4632b30cda9c16115db162ad771a58
Yatendra1915 <97654605+Yatendra1915@users.noreply.github.com>
2022-09-01 10:29:25 +0530

    Update SelectionSort.java

--- Sorting Algos/SelectionSort.java
+++ Sorting Algos/SelectionSort.java
@@ -1,19 +1,21 @@
-void selectionSort(int arr[], int n)
+class Sort{
+
+	void selectionSort(int arr[], int n)
 	{
 	    for(int i=0;i<n-1;i++){
 	        
 	        int min_idx=i;
         
-          for(int j=i+1;j<arr.length;j++){
+         	 for(int j=i+1;j<arr.length;j++){
             
-            if(arr[min_idx]>arr[j]){
-                min_idx=j;
-            }
-        }
+            		if(arr[min_idx]>arr[j]){
+                		min_idx=j;
+            		}
+        	}
 	        
 	        int temp = arr[min_idx];
 	        arr[min_idx]=arr[i];
 	        arr[i]=temp;
 	    }
 	}
-  
+}
