3f30141ab6931526757e4d2223bad117c5774b1e
Yatendra1915 <97654605+Yatendra1915@users.noreply.github.com>
2022-09-24 22:13:14 +0530

    Create Binary Search.java

---
+++ Binary Search/Easy/Binary Search.java
@@ -0,0 +1,25 @@
+class Solution {
+  /*
+    Binary search is a searching algorithm which is only applicable on sorted arrays
+  */
+    public int search(int[] nums, int target) {
+        
+        int low =0;
+        int high = nums.length-1;
+        
+        while(low<=high){
+            
+            int mid = low+(high-low)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+            
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        
+        return -1;
+    }
+}
