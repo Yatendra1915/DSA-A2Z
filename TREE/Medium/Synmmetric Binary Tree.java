9b80a65839b57510950863ece3d9e0e146249e9b
Yatendra1915 <97654605+Yatendra1915@users.noreply.github.com>
2023-03-25 00:29:11 +0530

    Create Synmmetric Binary Tree.java

---
+++ TREE/Medium/Synmmetric Binary Tree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        
+        if(root==null) return true;
+
+        return checkSymmetry(root.left,root.right);
+    }
+    
+    public boolean checkSymmetry(TreeNode root1,TreeNode root2){
+
+        if(root1==null && root2==null){
+            return true;
+        }
+
+        if(root1==null || root2==null){
+            return false;
+        }
+
+        return (root1.val==root2.val) && checkSymmetry(root1.right,root2.left) &&
+                                         checkSymmetry(root1.left,root2.right);
+    }
+}
